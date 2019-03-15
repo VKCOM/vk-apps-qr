b450f8dc992d2970281ee3f094e87c35153c773b
Anton Tsivarev <a.tsivarev@corp.vk.com>
2019-03-15 18:13:39 +0300

    PNG

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {Button, Div, FormLayout, Group, Input, ListItem, Panel, PanelHeader, View} from '@vkontakte/vkui';
+import {Button, Div, FormLayout, Group, Input, ListItem, Panel, PanelHeader, Select, View} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import qrCodeGenerator from '@vkontakte/vk-qr';
+import Download from '@axetroy/react-download';
+import Icon24Download from '@vkontakte/icons/dist/24/download';
+import {saveSvgAsPng} from 'save-svg-as-png';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            url: '',
+            url: 'https://vk.com',
+            size: '300',
         };
+
+        this.svgRef = React.createRef();
 
         this.onChange = this.onChange.bind(this);
     }
@@ -18,7 +24,7 @@ class App extends React.Component {
         let segs = qrCodeGenerator.QrSegment.makeSegments(url);
         let svg = qrCodeGenerator.QrCode.encodeSegments(segs, qrCodeGenerator.QrCode.Ecc.QUARTILE, 1, 40, -1, true).toSvgString();
 
-        return <span dangerouslySetInnerHTML={{__html: url ? svg : ''}}/>;
+        return <span ref={this.svgRef} dangerouslySetInnerHTML={{__html: url ? svg : ''}}/>;
     }
 
     onChange(e) {
@@ -27,27 +33,52 @@ class App extends React.Component {
     }
 
     render() {
-        const {url} = this.state;
+        const {url, size} = this.state;
+
+        let segs = qrCodeGenerator.QrSegment.makeSegments(url);
+        let svg = qrCodeGenerator.QrCode.encodeSegments(segs, qrCodeGenerator.QrCode.Ecc.QUARTILE, 1, 40, -1, true).toSvgString();
+
 
         return (
             <View activePanel="mainPanel">
                 <Panel id="mainPanel">
                     <PanelHeader>QR</PanelHeader>
-                    <FormLayout>
-                        <Input
-                            type="string"
-                            top="URL"
-                            name="url"
-                            value={url}
-                            onChange={this.onChange}
-                            status={url ? 'valid' : 'error'}
-                            bottom={url ? '' : 'Введите URL'}
-                        />
-                    </FormLayout>
-
                     <Group>
+                        <FormLayout>
+                            <Input
+                                type="string"
+                                top="URL"
+                                name="url"
+                                value={url}
+                                onChange={this.onChange}
+                                status={url ? 'valid' : 'error'}
+                                bottom={url ? '' : 'Введите URL'}
+                            />
+
+                            <Select top="Размер" name="size" onChange={this.onChange}>
+                                <option value="300">300x300</option>
+                                <option value="500">500x500</option>
+                                <option value="200">200x200</option>
+                                <option value="100">100x100</option>
+                            </Select>
+                        </FormLayout>
+
+                        <Div style={{
+                            width: size + 'px',
+                            height: size + 'px',
+                            margin: 'auto'
+                        }}>
+                            {this.svg(url, size)}
+                        </Div>
+
                         <Div>
-                            {this.svg(url)}
+                            <Button before={<Icon24Download/>} size="l" onClick={() => {
+                                saveSvgAsPng(this.svgRef.current.children[0], "png.png", {backgroundColor: '#fff'})
+                            }}>PNG</Button>
+                            <Download style={{display: 'inline'}} file="qr.svg" content={svg}>
+                                <Button before={<Icon24Download/>} size="l">SVG</Button>
+                            </Download>
+
                         </Div>
                     </Group>
                 </Panel>
