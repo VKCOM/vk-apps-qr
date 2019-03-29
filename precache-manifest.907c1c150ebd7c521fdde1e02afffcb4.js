4c0a4f950c5e39ada6b3baa7a9180ef62d3063f5
Anton Tsivarev <a.tsivarev@corp.vk.com>
2019-03-29 17:01:28 +0300

    Updates

---
+++ precache-manifest.907c1c150ebd7c521fdde1e02afffcb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9026db25152964052e",
+    "url": "/vk-apps-qr/static/js/runtime~main.d07b00a2.js"
+  },
+  {
+    "revision": "2d5db0506e897b5e1b30",
+    "url": "/vk-apps-qr/static/js/main.665e4582.chunk.js"
+  },
+  {
+    "revision": "d63a61a4b25b2d301654",
+    "url": "/vk-apps-qr/static/js/2.19d7a41b.chunk.js"
+  },
+  {
+    "revision": "d63a61a4b25b2d301654",
+    "url": "/vk-apps-qr/static/css/2.7dfc52c1.chunk.css"
+  },
+  {
+    "revision": "c48c92738a8f8239ad0b29a118c22723",
+    "url": "/vk-apps-qr/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-apps-qr/precache-manifest.775ea8cb000555b971b7dcdf9d503ce1.js"
+  "/vk-apps-qr/precache-manifest.907c1c150ebd7c521fdde1e02afffcb4.js"
 );
 
 workbox.clientsClaim();
