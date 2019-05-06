6df075215c8660d28f75d023b4af5204dc58c5f0
Anton Tsivarev <a.tsivarev@corp.vk.com>
2019-05-06 12:00:11 +0300

    Support the dark theme

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
 
+connect.subscribe((e) => {
+    switch (e.detail.type) {
+        case 'VKWebAppUpdateConfig':
+            let schemeAttribute = document.createAttribute('scheme');
+            schemeAttribute.value = e.detail.data.scheme ? e.detail.data.scheme : 'client_light';
+            document.body.attributes.setNamedItem(schemeAttribute);
+            break;
+
+        default:
+            console.log('Connect: ' + JSON.stringify(e));
+    }
+});
+
 // Init VK App
 connect.send('VKWebAppInit', {});
 
